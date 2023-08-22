92f0ede558b7694115929a513e841d8712535ae2
nlom0218 <nlom0218@naver.com>
2023-08-22 20:44:24 +0900

    feat: QueryClientProvider 설정

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
+import { useQuery } from '@tanstack/react-query';
+
 const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
   );
   return response.json();
 }
@@ -15,7 +17,9 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data } = useQuery(['posts'], fetchPosts);
+
+  if (!data) return <div>Loading...</div>;
 
   return (
     <>
