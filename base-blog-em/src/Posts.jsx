efab6c317d680c52cd54ccee7211c362c7b5954f
nlom0218 <nlom0218@naver.com>
2023-08-23 11:52:24 +0900

    feat: Pagination, Pre-fetching

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,28 +1,40 @@
-import { useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import { PostDetail } from './PostDetail';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(currentPage) {
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${currentPage}`
   );
-
-  if (!response.ok) throw Error('오마이갓!');
 
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts);
+  const queryClient = useQueryClient();
 
-  if (isLoading) return <h3>Loading...</h3>;
+  useEffect(() => {
+    if (currentPage >= maxPostPage) return;
+
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery({
+      queryKey: ['posts', nextPage],
+      queryFn: () => fetchPosts(currentPage),
+    });
+  }, [currentPage]);
+
+  // replace with useQuery
+  const { data, isError, error } = useQuery({
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    suspense: true,
+  });
 
   // error를 보여주기 전 react-query는 기본적으로 4번의 fetching를 하여 해당 데이터를 가져올 수 없다고 결정한다.
   if (isError)
@@ -47,16 +59,30 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <PostDetail post={selectedPost} />
+        </Suspense>
+      )}
     </>
   );
 }
